279a3554234c7c0305b65ad31f9a4836b4c225fd
afxal <afuthexal@gmail.com>
2024-09-08 03:22:01 +0800

    39

--- packages/wallie/vite.config.ts
+++ packages/wallie/vite.config.ts
@@ -1,15 +1,19 @@
-import react from "@vitejs/plugin-react-swc";
 import { resolve } from "path";
+import { terser } from "rollup-plugin-terser";
 import { defineConfig } from "vite";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 import dts from "vite-plugin-dts";
 import eslint from "vite-plugin-eslint";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
+import react from "@vitejs/plugin-react-swc";
 
 import * as packageJson from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    postcss: "./postcss.config.mjs",
+  },
   build: {
     lib: {
       entry: {
@@ -22,6 +26,7 @@ export default defineConfig({
         ...Object.keys(packageJson.peerDependencies),
         /^@ordzaar\/ordit-sdk\/.*/, // ordit-sdk/unisat, ordit-sdk/xverse imports
       ],
+      plugins: [terser()],
     },
     commonjsOptions: {
       include: [
